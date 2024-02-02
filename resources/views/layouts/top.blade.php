03e498b54cd92b6dae0bbe32ab0fde8a4476405e
orikooo3 <ori.kou.1025@gmail.com>
2024-02-02 22:42:47 +0900

    add: 食事記録と食品登録の各画面の作成

---
+++ resources/views/layouts/top.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ $title }}&nbsp;-&nbsp;{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="text-gray-900 antialiased">
+    <!-- Page Heading -->
+    @if (isset($header))
+        <header class="bg-main-color shadow md:z-40">
+            <div class="p-6 mx-auto text-xl font-semibold leading-tight text-gray-800">
+                {{ $header }}
+            </div>
+        </header>
+    @endif
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div>
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </div>
+
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
+    </div>
+</body>
+
+</html>
