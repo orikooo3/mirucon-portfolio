1446b0b07ab5c7f96d4cee0a998815a2d1a46e77
orikooo3 <ori.kou.1025@gmail.com>
2024-02-08 22:32:23 +0900

    add: 認証周りのviewを作成

--- resources/views/components/refer-button.blade.php
+++ resources/views/components/refer-button.blade.php
@@ -1,3 +1,4 @@
-<button>
-    //遷移ボタン
-</button>
+   <button
+       {{ $attributes->merge(['type' => 'submit', 'class' => 'group inline-flex justify-center items-center w-44 px-4 py-2 bg-main-color rounded-md font-semibold text-xs text-white-color uppercase tracking-widest shadow shadow-slate-500 hover:bg-main-hover-color hover:text-black-color']) }}>
+       {{ $slot }}
+   </button>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    <x-slot name="title">本日の記録</x-slot>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -9,7 +10,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{ Auth::user()->name }} さん、こんにちわ
                 </div>
             </div>
         </div>
