24fe1c81d5e8ae389ce5130ef20fb1f3bc0d79c5
orikooo3 <ori.kou.1025@gmail.com>
2024-02-17 21:00:19 +0900

    remove: ファイル名の変更

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\FoodRegistrationController;
+use App\Http\Controllers\MealRecordController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,6 +33,11 @@ Route::middleware('auth')->group(function () {
 
 // Route::resource('food_registration', FoodRegistrationController::class);
 
+/**
+ * 食品登録用ルーティング
+ * ・prefix: URLの接頭辞
+ * ・name: 名前付きルートの接頭辞
+ */
 Route::prefix('/food_registrations')
     ->middleware(['auth'])
     ->controller(FoodRegistrationController::class)
@@ -44,6 +51,26 @@ Route::prefix('/food_registrations')
         Route::get('/{id}/destroy', 'destroy')->name('destroy');
         Route::post('/{id}/destroy', 'destroy')->name('destroy');
     });
+
+/**
+ *  食事記録用のルーティング
+ */
+Route::prefix('/meal_records')
+    ->middleware(['auth'])
+    ->controller(MealRecordController::class)
+    ->name('meal_records.')
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/add', 'add')->name('add');
+        Route::get('/confirm', 'confirm')->name('confirm');
+        Route::post('/', 'store')->name('store');
+        Route::get('/{id}', 'show')->name('show');
+        Route::post('/{id}', 'update')->name('update');
+        Route::get('/{id}/destroy', 'destroy')->name('destroy');
+        Route::post('/{id}/destroy', 'destroy')->name('destroy');
+    });
+
+// Route::get('meals', [MealRecordController::class, 'index']);
 
 /**
  * 目的別にルート情報を分割する
