dc1d4db404d38157e8f2e9cf44e0042b0371a64f
orikooo3 <ori.kou.1025@gmail.com>
2024-02-18 23:09:58 +0900

    update: 主キーを削除

---
+++ database/migrations/2024_02_16_161434_create_food_registration_meal_record_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('food_registration_meal_records', function (Blueprint $table) {
+            // $table->id();
+            //外部キー制約
+            $table->foreignId('meal_record_id')->constrained()->onDelete('cascade');
+            //外部キー制約
+            $table->foreignId('food_registration_id')->constrained()->onDelete('cascade');
+            $table->primary(['meal_record_id', 'food_registration_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('food_registration_meal_records');
+    }
+};
