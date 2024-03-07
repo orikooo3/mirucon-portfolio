db6b269918bb23ae9470edbee4a6631838a136e7
orikooo3 <ori.kou.1025@gmail.com>
2024-03-07 16:35:00 +0900

    update: viewの編集。input numberの制限を最小値0にした

--- resources/views/food_registrations/create.blade.php
+++ resources/views/food_registrations/create.blade.php
@@ -1,61 +1,46 @@
 <x-app-layout>
     <x-slot name=title>食品登録</x-slot>
-    <section class="bg-white dark:bg-gray-900">
-        <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
-            <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new product</h2>
+    <section class="font-bold text-black-color dark:text-black-color">
+        <div class=" py-8 px-4 mx-auto max-w-2xl lg:py-16 bg-white-color dark:bg-white-color">
+            <h2 class="text-2xl mb-4">食品の登録</h2>
             <form action="{{ route('food_registrations.store') }}" method="POST">
                 @csrf
                 <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                     <div class="sm:col-span-2">
-                        <label for="name"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">食品名<span
-                                class="text-alarm-color">(必須)</span></label>
-                        <input type="text" name="food_name" id="food_name"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                            placeholder="Type product name" required="">
+                        <x-text-label for="food_name" class="block mb-2 font-medium"><x-slot name='min'>食品名<span
+                                    class="text-alarm-color">(必須)</span></x-slot></x-text-label>
+                        <x-text-input type="text" name="food_name" id="food_name" autocomplete="off" required />
                     </div>
                     <div class="w-full">
-                        <label for="brand"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">グラム<span
-                                class="text-alarm-color">(必須)</span></label>
-                        <input type="number" name="grams" id="grams"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                            placeholder="Product brand" required="">
+                        <x-text-label for="grams" class="block mb-2 font-medium"><x-slot name='min'>グラム<span
+                                    class="text-alarm-color">(必須)</span></x-slot></x-text-label>
+                        <x-text-input type="number" name="grams" id="grams" min="0" required />
                     </div>
                     <div class="w-full">
-                        <label for="price"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">カロリー<span
-                                class="text-alarm-color">(必須)</span></label>
-                        <input type="number" name="calory" id="calory"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                            placeholder="$2999" required="">
+                        <x-text-label for="calory" class="block mb-2 font-medium"><x-slot name='min'>カロリー<span
+                                    class="text-alarm-color">(必須)</span></x-slot></x-text-label>
+                        <x-text-input type="number" name="calory" id="calory" min="0" required />
                     </div>
                     <div class="sm:col-span-2">
-                        <label for="price"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">タンパク質</label>
-                        <input type="number" name="protein" id="protein"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                            placeholder="$2999">
+                        <x-text-label for="protein" class="block mb-2 font-medium"><x-slot
+                                name='min'>タンパク質</x-slot></x-text-label>
+                        <x-text-input type="number" name="protein" id="protein" min="0" />
                     </div>
                     <div div class="sm:col-span-2">
-                        <label for="item-weight"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">脂質</label>
-                        <input type="number" name="fat" id="fat"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                            placeholder="12">
+                        <x-text-label for="fat" class="block mb-2 font-medium"><x-slot
+                                name='min'>脂質</x-slot></x-text-label>
+                        <x-text-input type="number" name="fat" id="fat" min="0" />
                     </div>
                     <div class="sm:col-span-2">
-                        <label for="price"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">炭水化物</label>
-                        <input type="number" name="carbohydrate" id="carbohydrate"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                            placeholder="$2999">
+                        <x-text-label for="carbohydrate" class="block mb-2 font-medium"><x-slot
+                                name='min'>炭水化物</x-slot></x-text-label>
+                        <x-text-input type="number" name="carbohydrate" id="carbohydrate" min="0" />
                     </div>
                 </div>
-                <button type="submit"
-                    class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-                    登録
-                </button>
+                <x-submit-button type="submit"
+                    class="flex-col  items-center w-full  mt-9 inline-flex px-5 py-2.5 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                    <x-slot name='normal'>登録</x-slot>
+                </x-submit-button>
             </form>
         </div>
     </section>
